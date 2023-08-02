51fa6092676a9ba2f2a6a45ee76cb0b458140375
miticaandron <andron.mitica@gmail.com>
2023-08-02 06:27:23 +0300

    exercises divided into separate classes

---
+++ src/session1_homework/Exercise1.java
@@ -0,0 +1,18 @@
+package session1_homework;
+
+public class Exercise1 {
+    // 1. Simple Output Program: Write a program that prints you name, age, and favorite color on three separate lines.
+
+
+    public static void main (String[] args){
+
+        String Name = "Andron Mitica";
+        int age = 26;
+        String favoriteColor = "Blue";
+        char dot = '.';
+
+        System.out.println("My name is " + Name + dot);
+        System.out.println("I am " + age + " years old" + dot);
+        System.out.println("My favorite color is " + favoriteColor + dot);
+    }
+}
