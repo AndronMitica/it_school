6ed10b92d94dfd539da57e1cfd89bf51f48d78fb
miticaandron <andron.mitica@gmail.com>
2023-08-02 19:50:12 +0300

    update

--- src/session1_homework/Exercise3.java
+++ src/session1_homework/Exercise3.java
@@ -1,20 +1,13 @@
 package session1_homework;
-
 public class Exercise3 {
-    // 3. Operations: Write a program that performs various operations (addition, subtraction, multiplication, division, and modulus) on two numbers.
-    // The numbers can be hard-coded into the program. Print the result of each operation.
-
-    public static void main (String[] args){
-
-        double num1 = 7;
-        double num2 = 3;
-
-
-        System.out.println("The sum of the numbers is: " + (num1 + num2));
-        System.out.println("The subtraction of the numbers is: " + (num1 - num2));
-        System.out.println("The multiplication of the numbers is: " + (num1 * num2));
-        System.out.println("The division of the numbers is: " + (num1 / num2));
-        System.out.println("The modus of the numbers is: " + (num1 % num2));
-
+    /* 3. Operations: Write a program that performs various operations (addition, subtraction, multiplication, division, and modulus) on two numbers.The numbers can be hard-coded into the program. Print the result of each operation.*/
+    public static void main(String[] args){
+        double num1=7;
+        double num2=3;
+        System.out.println("The sum of the numbers is: "+(num1 + num2));
+        System.out.println("The subtraction of the numbers is: "+(num1 - num2));
+        System.out.println("The multiplication of the numbers is: "+(num1 * num2));
+        System.out.println("The division of the numbers is: "+(num1 / num2));
+        System.out.println("The modus of the numbers is: "+(num1 % num2));
     }
 }
