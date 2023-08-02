7ba6b1ba0f0cc1eaabde847787883843b8db5603
miticaandron <andron.mitica@gmail.com>
2023-08-02 19:57:40 +0300

    update code formatting

--- src/session1_homework/Exercise1.java
+++ src/session1_homework/Exercise1.java
@@ -1,13 +1,14 @@
 package session1_homework;
+
 public class Exercise1 {
     // 1. Simple Output Program: Write a program that prints you name, age, and favorite color on three separate lines.
-    public static void main(String[] args){
-        String name="Andron Mitica";
-        int age=26;
-        String favoriteColor="Blue";
-        char dot='.';
-        System.out.println("My name is "+name+dot);
-        System.out.println("I am "+age+" years old"+dot);
-        System.out.println("My favorite color is "+favoriteColor+dot);
+    public static void main(String[] args) {
+        String name = "Andron Mitica";
+        int age = 26;
+        String favoriteColor = "Blue";
+        char dot = '.';
+        System.out.println("My name is " + name + dot);
+        System.out.println("I am " + age + " years old" + dot);
+        System.out.println("My favorite color is " + favoriteColor + dot);
     }
 }
--- src/session1_homework/Exercise4.java
+++ src/session1_homework/Exercise4.java
@@ -1,12 +1,14 @@
 package session1_homework;
+
 import java.util.Scanner;
+
 public class Exercise4 {
     /* 4. Simple Input/Output: Write a program that asks the user to enter two numbers, perform addition on those numbers, and prints out the result.*/
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter two numbers: ");
-        int num1=myObj.nextInt();
-        int num2=myObj.nextInt();
-        System.out.println("The sum is "+(num1 + num2));
+        int num1 = myObj.nextInt();
+        int num2 = myObj.nextInt();
+        System.out.println("The sum is " + (num1 + num2));
     }
 }
