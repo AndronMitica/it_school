3458be93ddf58a8954ab6491e096a7e5ad00c18b
miticaandron <andron.mitica@gmail.com>
2023-08-13 19:05:46 +0300

    session 5 homework resolved

---
+++ src/session5/homework/MenuDrivenProgram.java
@@ -0,0 +1,38 @@
+package session5.homework;
+
+import java.util.Scanner;
+
+public class MenuDrivenProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Print \"Hello word\"");
+            System.out.println("2. Print your name");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Hello world ");
+                    break;
+                case 2:
+                    System.out.println("Enter your name: ");
+                    scanner.nextLine();
+                    String name = scanner.nextLine();
+                    System.out.println("Your name is: " + name);
+                    break;
+                case 3:
+                    System.out.println("Exiting ...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option. ");
+                    continue;
+            }
+        }
+    }
+}
---
+++ src/session5/homework/PrintNumberFromArray.java
@@ -0,0 +1,19 @@
+package session5.homework;
+
+/* 9. For-Each Loop
+ * Given an array of integers, write a program that prints each number in the array followed by "Even" if the number is even,
+ * or "Odd" if the number is odd. Utilize a for-each loop for this task.
+ * */
+public class PrintNumberFromArray {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 23, 11, 7, 32, 9, 6};
+
+        System.out.println("Number - Type");
+
+        for (int number : numbers) {
+            String type = (number % 2 == 0) ? "Even" : "Odd";
+            System.out.println(number + " " + type);
+        }
+    }
+}
