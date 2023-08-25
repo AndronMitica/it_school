2316b7b83c4f6cc6603e2dfc546e31051f692d00
miticaandron <andron.mitica@gmail.com>
2023-08-25 09:49:24 +0300

    codingkata session 3

--- src/codingkata/session3/Main.java
+++ src/codingkata/session3/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("Peek: " + myStak.peek());
     }
 }
+
 /*
  *
  * second element
--- src/codingkata/session3/Stack.java
+++ src/codingkata/session3/Stack.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Stack {
-    private List<String> stack;
+    private final List<String> stack;
 
     public Stack() {
         stack = new LinkedList<>();
